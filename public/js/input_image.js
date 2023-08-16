d27c5dcb23bfda34a4eb4e8c964cccad69160334
Aakash Thakur <thakurakash321@gmail.com>
2023-08-16 21:21:01 +0530

    feat: other esbuild features
    image, css, scss, watch/serve mode in esbuild

---
+++ public/js/input_image.js
@@ -0,0 +1,7 @@
+import png_url from "../images/A.png";
+const png_image = document.getElementById("image_png");
+png_image.src = png_url;
+
+import jpg_url from "../images/B.jpg";
+const jpg_image = document.getElementById("image_jpg");
+jpg_image.src = jpg_url;
--- src/App.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import ReactDOM from "react-dom";
-
-function App() {
-  return <div>Hello, esbuild!</div>;
-}
-
-ReactDOM.render(<App />, document.getElementById("root"));
