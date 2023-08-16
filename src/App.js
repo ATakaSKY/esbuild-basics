30e16f7af19a6789fff27ebe91eb99082031a6ab
Aakash Thakur <thakurakash321@gmail.com>
2023-08-16 19:58:50 +0530

    feat: added react
    React build with esbuild

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+function App() {
+  return <div>Hello, esbuild!</div>;
+}
+
+ReactDOM.render(<App />, document.getElementById("root"));
